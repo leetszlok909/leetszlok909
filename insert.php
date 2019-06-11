da58862dc7a488bc413fb6168ac1000ede11328a
leetszlok <leetszlok@icloud.com>
2019-06-11 16:33:02 +0800

    new

--- insert.php
+++
@@ -1,9 +0,0 @@
-<?PHP
-include("connect.php");
-$bankusername=$_POST['username'];
-$bankpassword=$_POST['password'];
- mysql_query($con,"INSERT Into user(bankusername,password) values(string,string)");
-
-	echo "Inserted";
-
-?>
